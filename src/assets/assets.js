e1aabdfd46ebdddc3c1d3c15adeca820d55d26c7
Niranjan Thilakaratna <nirandhammika@gmail.com>
2025-03-06 23:38:38 +0530

    change api url

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -22,7 +22,8 @@ export const images = {
     home,
 }
 
-export const baseUrl = "http://gt-backend:8080/gt/api/v1/";
+export const baseUrl = process.env.REACT_APP_API_URL + "/gt/api/v1/" || "http://gt-backend:8080/gt/api/v1/";
+// export const baseUrl = "http://gt-backend:8080/gt/api/v1/";
 
 export const sectionIdIelts = 1;
 export const sectionNameIelts = "IELTS";
