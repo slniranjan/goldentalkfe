61f53e3fc8f12384f57a1693f1db9965b75c4863
Niranjan Thilakaratna <nirandhammika@gmail.com>
2025-03-07 18:14:17 +0530

    teacher error handling

--- src/component/techers/teachers-ielts/TeacherRegisterIelts.jsx
+++ src/component/techers/teachers-ielts/TeacherRegisterIelts.jsx
@@ -6,7 +6,6 @@ import Select from 'react-select';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import React from 'react';
 
 const TeacherRegisterIelts = () => {
     const sectionName = sectionNameIelts;
