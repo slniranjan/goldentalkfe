2c69e3d0fd8ec74590947730b3297ffa07636ebc
Niranjan Thilakaratna <nirandhammika@gmail.com>
2025-03-07 19:21:28 +0530

    fix validation

--- src/component/students/students-ielts/StudentRegisterIelts.jsx
+++ src/component/students/students-ielts/StudentRegisterIelts.jsx
@@ -62,19 +62,9 @@ const StudentRegisterIelts = () => {
             newErrors.whatsAppNumber = "Enter a valid number (e.g., +94712345678).";
         }
 
-        console.log(formData.nic.length)
-
         if (!formData.nic.trim() || formData.nic.length < 10 || formData.nic.length > 12) {
             newErrors.nic = "NIC must be between 10 to 12 characters.";
         }
-
-        // if (!formData.nic) newErrors.nic = "NIC is required.";
-        // Check NIC
-        // if (!formData.nic.trim() || 10 <= formData.nic.length <= 12) {
-        //     newErrors.nic = "NIC must be between 10 to 12 characters.";
-        // }
-
-        
 
         if (!formData.email) {
             newErrors.email = "Email is required.";
